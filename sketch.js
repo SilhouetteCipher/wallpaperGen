b7aaeb011103ac0d735c1b361821c66fc731c73b
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2024-02-16 12:19:20 +0000

    main working

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+let width = 3840;
+let height = 2160;
+let colourArray = ["#ff6978", "#b1ede8", "#6E44FF", "#B892FF", "#00A388"];
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(width, height);
+  background("#fffcf9");
+  placeCircles(30);
+  filter(BLUR, 300);
 }
 
-function draw() {
-  background(220);
+function draw() {}
+
+function placeCircles(numOfCircles) {
+  for (var i = 0; i < numOfCircles; i++) {
+    fill(random(colourArray));
+    noStroke();
+    var x = random(width);
+    var y = random(height);
+    var r = random(1200, 1400);
+    ellipse(x, y, r, r);
+  }
 }
